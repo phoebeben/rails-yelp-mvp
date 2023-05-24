71b99a01eb0c8cebb8ef4a61a6b1d8fa107e548d
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-05-24 12:16:49 +0100

    created restaurants index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'restaurants#index'
+  get '/restaurants', to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+gloria = {name: "Gloria", address: "54-56 Great Eastern St, London EC2A 3QR", category: "french"}
+zizi =  {name: "Zizi", address: "5 Twickenham High St, Twickenham TW1 4LG", category: "italian"}
+wagamama = {name: "Wagamama", address: "5 Richmond Rd, Richmond, RT4 5FR", category: "japanese"}
+
+[dishoom, pizza_east, gloria, zizi, wagamama].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
