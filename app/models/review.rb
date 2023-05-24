4ca0aa59c25252930f27fcc65865942df8684f51
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-05-24 12:49:50 +0100

    changed review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, presence: true
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, numericality: { only_integer: true, in: (0..5) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/restaurants', to: 'restaurants#index'
   get 'restaurants/new', to: 'restaurants#new'
   post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show'
 end
