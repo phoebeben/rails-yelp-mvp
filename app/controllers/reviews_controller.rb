9e4cdebaeb7aebbbfb608b6f08f5375616749fa7
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-05-25 20:37:54 +0100

    implemented create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,7 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    # @review = Review.find(@restaurant.review.last)
     @review = @restaurant.reviews.build(review_params)
-    # @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
